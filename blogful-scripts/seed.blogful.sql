244d9410b82a5d6514766e6dd5b4c81d4d31787e
thanneman <thanneman1@gmail.com>
2020-01-14 20:05:38 -0700

    added blogful create and seed scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Blog Post 1',  now() - '45 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 2',  now() - '31 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 3',  now() - '30 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 4',  now() - '29 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 5',  now() - '29 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 6',  now() - '27 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 7',  now() - '27 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 8',  now() - '25 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 9',  now() - '25 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 10',  now() - '21 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 11',  now() - '21 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 12',  now() - '21 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 13',  now() - '18 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 14',  now() - '18 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 15',  now() - '14 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 16',  now() - '13 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 17',  now() - '10 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 18',  now() - '8 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 19',  now() - '5 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 20',  now() - '4 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'),
+    ('Blog Post 21',  now(), 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.')
+;
